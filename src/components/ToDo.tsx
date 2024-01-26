b366f049f7259385fd73d86a87cac445c4f33ede
choongnyeong6215 <choong6215@gmail.com>
2024-01-26 13:43:04 +0900

    카테고리 변경 처리

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -10,13 +10,25 @@ const ToDo = ({id, text, category} : ToDoListItfc) => {
     // 카테고리 변경 함수
     const handleChgCategory = (e : React.MouseEvent<HTMLButtonElement>) => {
         setToDos((prevToDoList) => {
-            return prevToDoList;
-        })
+            // 찾을 투두리스트 인덱스
+            const toDoIndex = prevToDoList.findIndex((toDo) => toDo.id === id);
+
+            // 과거 투두리스트
+            const prevToDo = prevToDoList[toDoIndex];
+            // 변경된 투두리스트
+            const newToDo = {id : id, text : text, category : e.currentTarget.name as ToDoListItfc["category"]};
+
+            // 변경 내역 적용
+            const updatedToDo = [...prevToDoList];
+            prevToDoList.splice(toDoIndex, 1, newToDo);
+            
+            return updatedToDo;
+        }) 
     };
 
     return (
         <ToDoLi>
-            <span>{text}</span>
+            <span>{text}</span>  
             {/* 현재 카테고리에 적합한 버튼 렌더링 */}
             {category !== "TO_DO" && <ToDoAddBtn name="TO_DO" onClick={handleChgCategory}>할 일</ToDoAddBtn>}
             {category !== "DOING" && <ToDoAddBtn name="DOING" onClick={handleChgCategory}>진행중</ToDoAddBtn>}
